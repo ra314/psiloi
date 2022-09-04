d69b73cf6f39bceb7b62824bcb4ec10fa2dc7c96
ra314 <rahulgandharva20122000@gmail.com>
2022-09-04 20:58:41 +1000

    Added a dictionary to store stationary attack implementations.

--- Scripts/Auto.gd
+++ Scripts/Auto.gd
@@ -4,8 +4,10 @@ var pos_to_unit_map: Dictionary = {}
 const BLOCKING_TILES = {0:true, -1:true}
 enum TEAM {UNSET, PLAYER, ENEMY}
 enum ATTACK {STAB, ARCHER, BOMBER, WIZARD, SHIELDBASH}
-var stationary_attacks = HashSet.neww([ATTACK.ARCHER, ATTACK.BOMBER, ATTACK.WIZARD, ATTACK.SHIELDBASH])
-var dynamic_attacks = HashSet.neww([ATTACK.STAB])
+var attack_enum_to_class_map = {ATTACK.ARCHER: ArcherStationaryAttack, \
+								ATTACK.BOMBER: BomberStationaryAttack, \
+								ATTACK.SHIELDBASH: ShieldBashStationaryAttack, \
+								ATTACK.WIZARD: WizardStationaryAttack}
 
 var all_units: Array
 var enemies_set: Dictionary
--- Scripts/Unit.gd
+++ Scripts/Unit.gd
@@ -11,14 +11,15 @@ var allowed_attack_enums: Dictionary
 var team_enum = AUTO.TEAM.UNSET
 var can_move := false
 var Main
-var stationary_attack_node: StationaryAttackInterface
+var stationary_attack_implementation: StationaryAttackInterface
 
 func init(tilemap: TileMap, grid_pos: Vector2, team_enum, allowed_attack_enums: Dictionary) -> Unit:
 	self.tilemap = tilemap
 	self.team_enum = team_enum
 	self.grid_pos = grid_pos
 	self.allowed_attack_enums = allowed_attack_enums
-	initialize_stationary_attack(allowed_attack_enums)
+	self.stationary_attack_implementation = \
+		get_stationary_attack_implementation(allowed_attack_enums).new()
 	
 	position = tilemap.map_to_world(grid_pos)
 	AUTO.pos_to_unit_map[grid_pos] = self
@@ -26,25 +27,11 @@ func init(tilemap: TileMap, grid_pos: Vector2, team_enum, allowed_attack_enums: 
 	return self
 
 # TODO: This doesn't work with multiple allowed stationary attacks
-func initialize_stationary_attack(allowed_attack_enums):
+func get_stationary_attack_implementation(allowed_attack_enums):
 	for attack_enum in allowed_attack_enums:
-		if attack_enum in AUTO.stationary_attacks:
-			if attack_enum == AUTO.ATTACK.ARCHER:
-				stationary_attack_node = ArcherStationaryAttack.new()
-			elif attack_enum == AUTO.ATTACK.BOMBER:
-				stationary_attack_node = BomberStationaryAttack.new()
-			elif attack_enum == AUTO.ATTACK.WIZARD:
-				stationary_attack_node = WizardStationaryAttack.new()
-			elif attack_enum == AUTO.ATTACK.SHIELDBASH:
-				stationary_attack_node = ShieldBashStationaryAttack.new()
-			else:
-				# Unimplemented stationary attack
-				assert(false)
-			return
-		elif attack_enum in AUTO.dynamic_attacks:
-			pass
-		else:
-			assert(false)
+		if attack_enum in AUTO.attack_enum_to_class_map:
+			return AUTO.attack_enum_to_class_map[attack_enum]
+	assert(false)
 
 # Return true if the movement is possible
 func move_with_bfs_to(end_grid_pos: Vector2) -> bool:
@@ -100,13 +87,15 @@ func action_done():
 		Main.TurnManager.increment_ply_count(team_enum)
 
 func stationary_attack(grid_pos) -> bool:
-	if stationary_attack_node == null:
+	if stationary_attack_implementation == null:
 		return false
-	if not (stationary_attack_node.get_attack_type() in allowed_attack_enums):
+	if not (stationary_attack_implementation.get_attack_type() in allowed_attack_enums):
 		return false
-	if stationary_attack_node.is_attack_highlight_on:
-		return stationary_attack_node.perform_attack(grid_pos, tilemap, self)
-	var possible_target_tiles = stationary_attack_node.get_possible_target_tiles(grid_pos)
-	stationary_attack_node.highlight_possible_target_tiles(possible_target_tiles, tilemap)
+	if stationary_attack_implementation.is_attack_highlight_on:
+		return stationary_attack_implementation.perform_attack(grid_pos, tilemap, self)
+	var possible_target_tiles = \
+		stationary_attack_implementation.get_possible_target_tiles(grid_pos)
+	stationary_attack_implementation.\
+		highlight_possible_target_tiles(possible_target_tiles, tilemap)
 	return false
 
